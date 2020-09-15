cf2a7f3e4afc62f37461ffc41b9452768dfb5629
epizarro98 <epizarro309@gmail.com>
2020-09-15 02:18:41 -0400

    Combine objects, arrays, and functions more than one level deep

---
+++ arrays_w_callbacks.js
@@ -0,0 +1,8 @@
+//Create a function knit that returns an object that has the following kinds of properties item: scarf, size : 6ft etc. Log a value of that object (hint: call the function and then call a property on the return value).
+const knit = {
+    newObject: {
+      item: 'scarf',
+      size: 3
+    }
+  }
+  console.log(knit.newObject.item);
