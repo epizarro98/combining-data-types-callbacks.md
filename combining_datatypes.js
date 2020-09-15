0fc9ec06310cb0629a971334fa7692715656f9c3
epizarro98 <epizarro309@gmail.com>
2020-09-15 00:18:02 -0400

    Combine objects, arrays, and functions

---
+++ combining_datatypes.js
@@ -0,0 +1,25 @@
+// Create an array called apartmentBuilding that has an array as one of 
+// its elements, the inner array should be the names of the tenants. 
+// Log one of the elements of the inner array.
+const crayonBox = {
+    crayons: ['red', 'blue', 'purple', 'pink']
+   }
+   console.log(crayonBox.crayons[0])
+   
+   
+   const bottle = {
+     cap:  {
+       material: 'plastic',
+       color: 'blue'
+     }
+   }
+   console.log(bottle.cap.material)
+   
+   
+   const receipt = {name: 'ed', price: 5}
+   console.log(receipt.name)
+   
+   const apartmentBuilding = {
+     tenants: ['bob', 'rick', 'kelsey']
+   }
+   console.log(apartmentBuilding.tenants[1])
